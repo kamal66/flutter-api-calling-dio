5fa28624b767dcb432fc5c56a026e8dad8805306
Kamal Nathani <kamal66patel@gmail.com>
2024-06-05 12:12:31 +0530

    Update api.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -2,5 +2,5 @@ class Api {
   static const baseUrl = "https://fakestoreapi.com/";
   static const baseUrlMovies = "https://api.themoviedb.org/3/";
 
-  static const api_key = "5e3459baab5bedd6978046bea4ec8886";
+  static const api_key = "";
 }
